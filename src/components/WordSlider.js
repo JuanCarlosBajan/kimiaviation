e0b2a077a3bb69232ea0523d098ad3fea81c6ef6
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-20 13:24:18 -0600

    Word Slider

---
+++ src/components/WordSlider.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import '../styles/Content.scss'
+
+
+
+
+const WordSlider = () => {
+
+    return (<>
+    
+    <div class="slider2">
+        <div class="slide-track">
+
+            <div class="slide">
+                    <h1 className=' slider_text'>
+                    Dubai<em className='resalted_word'><strong>15°C</strong></em>
+                    </h1>   
+
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    London<em className='resalted_word'><strong>17°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Syndey<em className='resalted_word'><strong>15°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Toronto<em className='resalted_word'><strong>19°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Paris<em className='resalted_word'><strong>12°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Prague<em className='resalted_word'><strong>21°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Vienna<em className='resalted_word'><strong>16°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+            <div class="slide">
+            
+                    <h1 className='slider_text'>
+                    Venice<em className='resalted_word'><strong>15°C</strong></em>
+                    </h1>   
+                
+            </div>
+            <div class="slide_inclined">
+                <div class="inclined"></div>
+            </div>
+        
+        
+
+        
+
+        </div>
+    </div>
+    
+    </>)
+
+}
+
+export default WordSlider;
