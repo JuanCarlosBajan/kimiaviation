547afd0d7b03f0ea40ca5e54cf1021ec6c100a19
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-20 13:22:51 -0600

    window change

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import '../styles/Content.css';
+import '../styles/Content.scss';
 
 const Window = () => {
 
     return (
+        <>
         <div className="Window_Container">
                 <video autoPlay muted loop id="videoScene">
                     <source src={'https://player.vimeo.com/external/565526339.hd.mp4?s=d76a061e031cc8d0cb4998060fe7d753e0878231&profile_id=174'} type="video/mp4"></source>    
@@ -11,6 +12,15 @@ const Window = () => {
                 <div className="Window">
                 </div>
         </div>
+        <div className='quote'>
+            <h1 className='quote_text'>
+                Explore
+                <br/>the <em><strong>new force
+                </strong></em><br/>in the private
+                <br/><em><strong>jet industry</strong></em>
+            </h1>
+        </div>
+        </>
     )
 
 }
