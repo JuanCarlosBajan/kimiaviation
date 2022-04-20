524d410a64420963f43141dc31d516713840caf0
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-20 13:24:07 -0600

    Slider

---
+++ src/components/Slider.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Slider = () => {
+
+    return (<>
+    
+    <div class="slider">
+        <div class="slide-track">
+        <div class="slide1">
+            <div className='priorities_picture1'></div>
+		</div>
+		<div class="slide2">
+            <div className='priorities_picture2'></div>
+		</div>
+        <div class="slide3">
+            <div className='priorities_picture3'></div>
+		</div>
+        <div class="slide1">
+            <div className='priorities_picture1'></div>
+		</div>
+		<div class="slide2">
+            <div className='priorities_picture2'></div>
+		</div>
+        <div class="slide3">
+            <div className='priorities_picture3'></div>
+		</div>
+        <div class="slide1">
+            <div className='priorities_picture1'></div>
+		</div>
+		<div class="slide2">
+            <div className='priorities_picture2'></div>
+		</div>
+        <div class="slide3">
+            <div className='priorities_picture3'></div>
+		</div>
+        <div class="slide1">
+            <div className='priorities_picture1'></div>
+		</div>
+		<div class="slide2">
+            <div className='priorities_picture2'></div>
+		</div>
+        <div class="slide3">
+            <div className='priorities_picture3'></div>
+		</div>
+        </div>
+    </div>
+
+    
+    
+    </>)
+
+}
+
+export default Slider;
