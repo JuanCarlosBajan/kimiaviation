179ec6a7b750706005308d5f041b0dbc06a5273c
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-20 13:23:46 -0600

    Quote

---
+++ src/components/Quote.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/Content.scss'
+
+const Quote = () => {
+
+    return (
+        <>
+        
+        <div className='phrase_container'>
+
+            <h1 className='quote_text'>
+            We work tirelessly to<br/>
+            make your trip an <br/>
+            <em className='resalted_word'><strong>unforgettable</strong></em> experience
+            </h1>
+
+
+        </div>
+
+        <div className='priorities_picture_container'>
+            <div class="line"></div>
+        </div>
+
+        </>
+    )
+
+}
+
+export default Quote;
