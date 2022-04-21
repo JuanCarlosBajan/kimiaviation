c6917ddaf7bddef1e4c61615355d188ac3345f88
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-04-21 12:55:37 -0600

    changes sky

--- src/components/Sky.js
+++ src/components/Sky.js
@@ -5,12 +5,13 @@ const Sky = () => {
     
     return( 
 
+        <>
         <div className="Video_Container" id="video">
                 <video className='Video_Container2' autoPlay muted loop id="videoSky">
                     <source src={'https://player.vimeo.com/external/563130035.hd.mp4?s=5bd311f698519ffa90aa5136b96d1e86aa78d107&profile_id=175'} type="video/mp4"></source>    
                 </video>
         </div>
-
+        </>
     )
 
 }
